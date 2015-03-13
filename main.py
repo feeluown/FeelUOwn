c03c225bdd6630dc0b6f1dbffe37a82b4ad91fb2
ysw <yinshaowen241@gmail.com>
2015-03-13 09:50:35 +0800

    fix desktop icon bug

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 __author__ = 'cosven'
 
 import sys
+import os
 from PyQt4.QtGui import *
 from controllers import MainWidget
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = MainWidget()
+    path = sys.path[0]
+    os.chdir(path)
     # float center
+    w = MainWidget()
     w.move((QApplication.desktop().width() - w.width())/2,(QApplication.desktop().height() - w.height())/2)
     w.show()
     sys.exit(app.exec_())
