deb7bc280e20487e44f973a2b33803f2a0f37bd9
cosven <yinshaowen241@gmail.com>
2015-04-16 15:35:11 +0800

    add sh hint

--- generateSphinxDoc.sh
+++ generateSphinxDoc.sh
@@ -6,3 +6,5 @@ sphinx-apidoc -o ./sphinx_doc/source/ ./
 mv ./sphinx_doc/source/index.rst.bak ./sphinx_doc/source/index.rst
 cd sphinx_doc
 make html
+
+echo "你可以在sphinx_doc/build/html目录下打开index.html文件"
