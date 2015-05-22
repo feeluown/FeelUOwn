db8f5f6a89c7489e7243231ea68fad755a7dcc2a
ysw <yinshaowen241@gmail.com>
2015-05-22 09:00:48 +0800

    hard to design, shit

---
+++ src/main.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+# -*- coding:utf8 -*-
+__author__ = 'cosven'
+
+import sys
+import os
+from PyQt4.QtGui import *
+from controllers import MainWidget, LoginDialog
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('icons/format.ico'))
+    path = sys.path[0]
+    os.chdir(path)
+
+    qss = "themes/default.qss"
+    with open(qss, "r") as qssfile:
+        app.setStyleSheet(qssfile.read())
+    w = MainWidget()
+    w.move((QApplication.desktop().width() - w.width())/2,(QApplication.desktop().height() - w.height())/2)
+    w.show()
+    sys.exit(app.exec_())
+
