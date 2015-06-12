d3c2f4979f84f0f9c21205b440d90d108be6a2a9
ysw <yinshaowen241@gmail.com>
2015-06-12 11:58:50 +0800

    webkit: 流程走通

---
+++ src/widgets/js_interface.py
@@ -0,0 +1,5 @@
+# -*- coding: utf8 -*-
+
+
+def js_test():
+    pass
--- src/widgets/webview.py
+++ src/widgets/webview.py
@@ -9,38 +9,32 @@ from setting import MODE, PUBLIC_PATH, DEBUG
 import json
 
 
-
-testJs = """
-window.python_log(%s);
-"""
-
-
 class WebView(QWebView):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.load_css()
         self.init()
         self.loadFinished.connect(self.on_load_finished)
-
-
 
     def init(self):
         if MODE == DEBUG:
             self.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
 
-
-
-
     def load_css(self):
         all_css = QFileInfo(PUBLIC_PATH + 'all.css').absoluteFilePath()
-        print(all_css)
-        # self.settings().setUserStyleSheetUrl(QUrl.fromLocalFile(all_css))
-
+        self.settings().setUserStyleSheetUrl(QUrl.fromLocalFile(all_css))
 
     @pyqtSlot()
     def on_load_finished(self):
         print('finish')
         print(self.page())
         a = {'a': 'hello'}
-        self.page().mainFrame().evaluateJavaScript(testJs % json.dumps(a))
+        self.page().mainFrame().evaluateJavaScript('')
+
+    def run_js_interface(self, data=None):
+        """
+
+        :param data: string, such as json.dumps(dict)
+        :return:
+        """
+        pass
