59f75da338721b2683df99787eba1168bc741e65
oryxfea <oryxfea@gmail.com>
2015-10-13 09:48:45 +0800

    Replace some magic number with constant variable

--- src/base/player.py
+++ src/base/player.py
@@ -36,6 +36,11 @@ class Player(QMediaPlayer):
         self._app_event_loop = asyncio.get_event_loop()
         self._music_error_times = 0
 
+        # latency of retying next operation when error happened
+        self._RETRY_LATENCY = 3
+        # when _music_error_times reached _MUSIC_ERROR_MAXIMUM, play next music
+        self._MUSIC_ERROR_MAXIMUM = 3
+
     def change_player_mode(self):
         """fm 和 正常两种模式切换"""
         self._fm_mode = ~self._fm_mode
@@ -173,16 +178,15 @@ class Player(QMediaPlayer):
             else:
                 LOG.error(u'播放器出现error, 类型为' + str(error))
         if error == QMediaPlayer.NetworkError:
-            latency = 3
-
-            if self._music_error_times >= 3 or self._current_index < 0 or self._current_index >= len(self._music_list):
+            if self._music_error_times >= self._MUSIC_ERROR_MAXIMUM or \
+                    self._current_index < 0 or self._current_index >= len(self._music_list):
                 self._music_error_times = 0
-                self._app_event_loop.call_later(latency, self.play_next)
-                LOG.error(u'播放器出现错误：网络连接失败，{}秒后尝试播放下一首'.format(latency))
+                self._app_event_loop.call_later(self._RETRY_LATENCY, self.play_next)
+                LOG.error(u'播放器出现错误：网络连接失败，{}秒后尝试播放下一首'.format(self._RETRY_LATENCY))
             else:
                 self._music_error_times += 1
-                self._app_event_loop.call_later(latency, self.play, self._music_list[self._current_index])
-                LOG.error(u'播放器出现错误：网络连接失败, {}秒后重试'.format(latency))
+                self._app_event_loop.call_later(self._RETRY_LATENCY, self.play, self._music_list[self._current_index])
+                LOG.error(u'播放器出现错误：网络连接失败, {}秒后重试'.format(self._RETRY_LATENCY))
         elif error == QMediaPlayer.ResourceError:
             LOG.error(u'播放器出现错误：缺少解码器')
         return
