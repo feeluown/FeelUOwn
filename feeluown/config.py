dd0b66a9833c4772767802bf5c2e89fe773eec51
Cosven <yinshaowen241@gmail.com>
2016-02-14 11:43:51 +0800

    complete config module

--- feeluown/config.py
+++ feeluown/config.py
@@ -1,12 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from collections.abc import MutableMapping
 
-class Config(object):
+import yaml
+
+from feeluown.constants import CONFIG_FILE_PATH
+
+
+class Config(MutableMapping):
     def __init__(self):
-        pass
+        self._data = dict()
 
-    def read(self):
-        pass
+    def load(self, path=CONFIG_FILE_PATH):
+        with open(path, 'r') as f:
+            self._data.update(yaml.load(f))
 
-    def write(self):
-        pass
+    def save(self, path=CONFIG_FILE_PATH):
+        with open(path, 'w') as f:
+            f.write(yaml.dump(self._data))
+
+    def __getitem__(self, key):
+        return self._data[self.__keytransform__(key)]
+
+    def __setitem__(self, key):
+        return self._data[self.__keytransform__(key)]
+
+    def __keytransform__(self, key):
+        return key
+
+    def __delitem__(self, key):
+        del self._data[self.__keytransform__(key)]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return iter(self._data)
+
+
+config = Config()
