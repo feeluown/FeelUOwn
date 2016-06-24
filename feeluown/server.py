6fcaf0a2c0dda03e98b537bd6158fdff05d2b9d5
ysw <yinshaowen241@gmail.com>
2016-06-24 14:39:26 +0800

    use signal instead of direct call

--- feeluown/server.py
+++ feeluown/server.py
@@ -2,13 +2,27 @@ import asyncio
 import logging
 import socketserver
 
+from PyQt5.QtCore import QObject, pyqtSignal
+
 logger = logging.getLogger(__name__)
 
 
-class Server(object):
+class Server(QObject):
+    play_signal = pyqtSignal()
+    pause_signal = pyqtSignal()
+    play_or_pause_signal = pyqtSignal()
+    play_next_signal = pyqtSignal()
+    play_previous_signal = pyqtSignal()
+
     def __init__(self, app):
-        super().__init__()
+        super().__init__(app)
         self._app = app
+
+        self.play_signal.connect(self._app.player.play)
+        self.pause_signal.connect(self._app.player.pause)
+        self.play_or_pause_signal.connect(self._app.player.play_or_pause)
+        self.play_next_signal.connect(self._app.player.play_next)
+        self.play_previous_signal.connect(self._app.player.play_last)
 
     def run(self):
         host = '0.0.0.0'
@@ -19,7 +33,7 @@ class Server(object):
             logger.error('udp %d port already in use' % port)
             return
 
-        self.server.app = self._app
+        self.server.ctrl = self
         event_loop = asyncio.get_event_loop()
         event_loop.run_in_executor(None,
                                    self.server.serve_forever)
@@ -36,12 +50,14 @@ class Handler(socketserver.BaseRequestHandler):
         logger.info('{} send: {}'.format(self.client_address[0], self.data))
         command = self.data.decode('utf-8')
         if command == 'play':
-            self.server.app.player.play()
+            self.server.ctrl.play_signal.emit()
         elif command == 'pause':
-            self.server.app.player.pause()
+            self.server.ctrl.pause_signal.emit()
         elif command == 'next':
-            self.server.app.player.play_next()
+            self.server.ctrl.play_next_signal.emit()
         elif command == 'previous':
-            self.server.app.player.play_last()
+            self.server.ctrl.play_previous_signal.emit()
+        elif command == 'play_pause':
+            self.server.ctrl.play_or_pause_signal.emit()
         else:
             logger.warning('command not found: %s' % command)
