51462bb8ef7c409ef7c1751b1a8b1cfecc62bc57
cosven <yinshaowen241@gmail.com>
2019-01-01 20:20:23 +0800

    bump version to 2.3

--- feeluown/__init__.py
+++ feeluown/__init__.py
@@ -6,7 +6,7 @@ import logging.config
 from .consts import LOG_FILE
 
 
-__version__ = '2.3a1'
+__version__ = '2.3'
 
 
 dict_config = {
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # FIXME depends on PyQt5
     install_requires=[
         'quamash>=0.5.5',
-        'fuocore>=2.3a0',
+        'fuocore>=2.3',
         'requests',
         ],
     tests_require=['pytest'],
