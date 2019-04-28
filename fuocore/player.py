7f265fd5450b457aeb0a5f659f4fb6a54373c9e9
cosven <yinshaowen241@gmail.com>
2019-04-28 19:13:24 +0800

    fix re-init playlist

--- fuocore/player.py
+++ fuocore/player.py
@@ -257,7 +257,8 @@ class AbstractPlayer(metaclass=ABCMeta):
     def __init__(self, playlist=None, **kwargs):
         self._position = 0  # seconds
         self._volume = 100  # (0, 100)
-        self._playlist = playlist or Playlist()
+        if playlist is not None:
+            self._playlist = Playlist() if playlist is None else playlist
         self._state = State.stopped
         self._duration = None
 
