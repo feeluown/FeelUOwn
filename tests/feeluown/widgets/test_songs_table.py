7528872d75a7641bb54ce81157a589e9318e764e
cosven <yinshaowen241@gmail.com>
2019-06-27 02:06:36 +0800

    fix: skip qt tests in travis env

--- tests/feeluown/widgets/test_songs_table.py
+++ tests/feeluown/widgets/test_songs_table.py
@@ -1,8 +1,14 @@
 import time
 
+import pytest
+
+from tests.helpers import is_travis_env
 from feeluown.widgets.songs_table_meta import SongsTableMetaWidget
 
 
+# TODO: use xvfb in travis env
+# example: https://github.com/pytest-dev/pytest-qt/blob/master/.travis.yml
+@pytest.mark.skipif(is_travis_env, reason="travis env has no display")
 def test_songs_table_meta(qtbot):
     widget = SongsTableMetaWidget()
     widget.title = '我喜欢的音乐'
