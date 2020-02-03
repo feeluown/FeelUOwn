a50a387cc5a7c7cddf4d48c5aadafd996ef0b6cc
Xiaoxing Ye <ye@xiaoxing.us>
2020-02-03 23:49:55 +1000

    config: refine setup.py
    Includes README.md as long description.

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from os import path
 
 import feeluown
+
+current_directory = path.abspath(path.dirname(__file__))
+with open(path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='feeluown',
     version=feeluown.__version__,
     description='*nix music player',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="GPL-3.0",
     author='Cosven',
     author_email='yinshaowen241@gmail.com',
     packages=[
@@ -22,7 +30,7 @@ setup(
         'fuocore.cmds',
         'fuocore.models',
         'fuocore.protocol',
-        ],
+    ],
     py_modules=['mpv'],
     package_data={
         '': ['*.qss',
@@ -32,24 +40,30 @@ setup(
              '../icons/*.icns',
              'themes/*.qss',
              ]
-        },
-    url='https://github.com/cosven/FeelUOwn',
+    },
+    python_requires=">=3.5",
+    url='https://github.com/feeluown/FeelUOwn',
     keywords=['media', 'player', 'application', 'PyQt5', 'Python 3'],
-    classifiers=(
+    classifiers=[
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
         'Environment :: X11 Applications :: Qt',
-        ),
+        "Topic :: Multimedia :: Sound/Audio",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
     # FIXME depends on PyQt5
     install_requires=[
         'quamash>=0.5.5',
         'janus',
         'requests',
         'pyopengl',
-        ],
+    ],
     extras_require={
         'battery': ['fuo-local>=0.1.1',
                     'fuo-xiami>=0.1.2',
@@ -66,10 +80,10 @@ setup(
                    'pytest-mock'],
     entry_points={
         'console_scripts': [
-                "feeluown=feeluown.__main__:main",
-                "fuo=feeluown.__main__:main",
-                "feeluown-genicon=feeluown.install:generate_icon",
-                # "feeluown-update=feeluown.install:update"
-            ]
-        },
-    )
+            "feeluown=feeluown.__main__:main",
+            "fuo=feeluown.__main__:main",
+            "feeluown-genicon=feeluown.install:generate_icon",
+            # "feeluown-update=feeluown.install:update"
+        ]
+    },
+)
