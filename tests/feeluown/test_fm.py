e9aae6d2bd729b1d7d5ed5c92e32ff7073ad48da
cosven <yinshaowen241@gmail.com>
2020-02-08 14:00:47 +0800

    tests: add more unittests for FM

--- tests/feeluown/test_fm.py
+++ tests/feeluown/test_fm.py
@@ -1,5 +1,12 @@
+import asyncio
+
+import pytest
+
+from fuocore.excs import ProviderIOError
+
 from feeluown.fm import FM
 from feeluown.player import Playlist, PlaylistMode
+from feeluown.task import TaskManager
 
 
 def test_fm_activate_and_deactivate(app_mock, song, mocker):
@@ -21,3 +28,47 @@ def test_when_playlist_fm_mode_exited(app_mock, song, mocker):
     fm.activate(mock_fetch)
     app_mock.playlist.mode = PlaylistMode.normal
     assert fm._activated is False  # noqa
+
+
+@pytest.mark.asyncio
+async def test_fetch_song_cancelled(event_loop, app_mock, song, mocker):
+    mock_fetch = mocker.MagicMock()
+    app_mock.playlist = Playlist(app_mock)
+    app_mock.task_mgr = TaskManager(app_mock, event_loop)
+    fm = FM(app_mock)
+    fm.activate(mock_fetch)
+    task_spec = app_mock.task_mgr.get_or_create(fm._fetch_songs_task_name)
+    task_spec._task.cancel()
+    await asyncio.sleep(0.1)  # schedule cancel callback
+    assert fm._is_fetching_songs is False
+
+
+@pytest.mark.asyncio
+async def test_fetch_song_failed(event_loop, app_mock, song, mocker):
+    mock_fetch = mocker.MagicMock(side_effect=ProviderIOError)
+    mock_fm_add = mocker.patch.object(Playlist, 'fm_add')
+    app_mock.playlist = Playlist(app_mock)
+    app_mock.task_mgr = TaskManager(app_mock, event_loop)
+    fm = FM(app_mock)
+    fm.activate(mock_fetch)
+    assert fm._is_fetching_songs is True
+    await asyncio.sleep(0.1)  # schedule mock_fetch callback
+    assert fm._is_fetching_songs is False
+    assert not mock_fm_add.called
+
+
+@pytest.mark.asyncio
+async def test_multiple_eof_reached_signal(event_loop, app_mock, song, mocker):
+    mock_fetch = mocker.MagicMock(return_value=[song] * 3)
+    mock_fm_add = mocker.patch.object(Playlist, 'fm_add')
+    app_mock.playlist = Playlist(app_mock)
+    app_mock.task_mgr = TaskManager(app_mock, event_loop)
+    fm = FM(app_mock)
+    fm.activate(mock_fetch)
+    app_mock.playlist.next()
+    app_mock.playlist.next()
+    task_spec = app_mock.task_mgr.get_or_create(fm._fetch_songs_task_name)
+    await task_spec._task  # this is a little bit tricky
+    # called exactly once
+    mock_fetch.assert_called_once_with(3)
+    assert mock_fm_add.called
