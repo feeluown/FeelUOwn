8b05f877b0663ea3d863a8523390fa686b21991f
cosven <yinshaowen241@gmail.com>
2020-03-08 11:49:39 +0800

    feat: add macos dark colors

--- feeluown/theme.py
+++ feeluown/theme.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import json
 import os
+import sys
+from collections import defaultdict
 
-from PyQt5.QtGui import QPalette
+from PyQt5.QtGui import QGuiApplication, QPalette, QColor
 from PyQt5.QtWidgets import QApplication
 from fuocore.utils import is_osx, get_osx_theme
 
@@ -13,8 +16,16 @@ logger = logging.getLogger(__name__)
 DARK = 'dark'
 LIGHT = 'light'
 
+Roles = ['AlternateBase', 'Background', 'Base', 'BrightText',
+         'Button', 'ButtonText', 'Dark', 'Foreground', 'Highlight',
+         'HighlightedText', 'Light', 'Link', 'LinkVisited', 'Mid',
+         'Midlight', 'NoRole', 'PlaceholderText', 'Shadow', 'Text',
+         'ToolTipBase', 'ToolTipText', 'Window', 'WindowText']
 
-def read_qss(filename):
+Groups = ['Disabled', 'Active', 'Inactive']
+
+
+def read_resource(filename):
     filepath = os.path.abspath(__file__)
     dirname = os.path.dirname(filepath)
     qssfilepath = os.path.join(dirname, 'themes/{}'.format(filename))
@@ -34,6 +45,13 @@ class ThemeManager:
     def __init__(self, app):
         self._app = app
         self.theme = None
+
+    def initialize(self):
+        # XXX: I don't know why we should autoload twice
+        # to make it work well on Linux(GNOME)
+        self.autoload()
+        self._app.initialized.connect(lambda app: self.autoload(), weak=False)
+        QApplication.instance().paletteChanged.connect(self.autoload)
 
     def autoload(self):
         if self._app.config.THEME == 'auto':
@@ -62,17 +80,52 @@ class ThemeManager:
         return DARK
 
     def load_light(self):
-        common = read_qss('common.qss')
-        light = read_qss('light.qss')
+        common = read_resource('common.qss')
+        light = read_resource('light.qss')
         QApplication.instance().setStyleSheet(common + light)
 
     def load_dark(self):
-        common = read_qss('common.qss')
-        dark = read_qss('dark.qss')
+        common = read_resource('common.qss')
+        dark = read_resource('dark.qss')
         QApplication.instance().setStyleSheet(common + dark)
 
-        # 测试发现，在 macOS dark 模式下，alternate 行的背景色是灰色，
-        # 正确的颜色应该类似透明的黑色。我们这里手动给 SongsTableView 处理这种情况。
-        # 复现代码见：https://gist.github.com/cosven/0d1f06cb78c79171da51bee9376f71fd
-        self._app.ui.songs_table.setStyleSheet(
-            "alternate-background-color: rgba(50,50,50,0.2)")
+        if sys.platform.lower() == 'linux':
+            # So many DEs on Linux! Hard to compat! We give them macOS
+            # dark theme colors!
+            #
+            # Users can also design a theme colors by themselves,
+            # we provider dump_colors/load_colors function for conviniece.
+            content = read_resource('macos_dark.colors')
+            colors = json.loads(content)
+            try:
+                QApplication.instance().paletteChanged.disconnect(self.autoload)
+            except TypeError:
+                pass
+            palette = load_colors(colors)
+            self._app.setPalette(palette)
+            QApplication.instance().paletteChanged.connect(self.autoload)
+
+
+def dump_colors():
+    json_ = defaultdict(dict)
+    palette = QGuiApplication.palette()
+    for group_attr in Groups:
+        group = getattr(QPalette, group_attr)
+        for role_attr in Roles:
+            role = getattr(QPalette, role_attr)
+            json_[group_attr][role_attr] = palette.color(group, role).name()
+    return json_
+
+
+def load_colors(colors):
+    palette = QGuiApplication.palette()
+    for group_attr, value in colors.items():
+        for role_attr, color_name in value.items():
+            try:
+                role = getattr(QPalette, role_attr)
+                group = getattr(QPalette, group_attr)
+                palette.setColor(group, role, QColor(color_name))
+            except AttributeError:
+                pass
+    QGuiApplication.setPalette(palette)
+    return palette
