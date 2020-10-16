978afb977cf45eb111b8c2d81589c0f19af5412f
wuliaotc <372072553@qq.com>
2020-10-16 23:15:08 +0800

    feat:Modify the logic of field parsing and dumping

--- fuocore/models/uri.py
+++ fuocore/models/uri.py
@@ -18,6 +18,8 @@ Currently, there is not way to achieve this.
 
 import asyncio
 import re
+import logging
+from enum import Enum
 
 from fuocore.models import ModelType, ModelExistence
 from fuocore.provider import dummy_provider
@@ -61,28 +63,106 @@ class Resolver:
         cls.loop = asyncio.get_event_loop()
 
 
-def _model_unescape(model_str):
-    # \- => -
-    return model_str.replace('\\-', '-').replace('\\\\', '\\')
+# def _model_unescape(model_str):
+#     # \- => -
+#     return model_str.replace('\\-', '-').replace('\\\\', '\\')
+#
+#
+# def _model_escape(model_str):
+#     # - => \-
+#     return model_str.replace('\\', '\\\\').replace('-', '\\-')
+#
+
+def _field_unescape(filed: str) -> str:
+    # '"\" \" \\"' => '" " \'
+    if len(filed) >= 2 and filed.startswith('"') and filed.endswith('"'):
+        filed = filed.replace('\\"', '"').replace('\\\\', '\\')
+    return filed
 
 
-def _model_escape(model_str):
-    # - => \-
-    return model_str.replace('\\', '\\\\').replace('-', '\\-')
+def _field_escape(filed: str) -> str:
+    # 如果字段中存在'-',则进行转义
+    if filed.find('-') != -1:
+        filed = filed.replace('\\', '\\\\').replace('"', '\\"')
+        return '"{}"'.format(filed)
+    return filed
 
 
-def _split(s, num):
-    # 这里使用'- '的原因是为了解决:无网络条件下无法获取在线歌曲时长,
-    # 导致写出格式存在问题,之前的_split无法解析下列异常情况
-    # 'fuo://xiami/songs/1769834090	# Flower Dance - DJ OKAWARI -  -'
-    # FIXME:修改写入部分的代码,而不是hack parse 过程
-    DELIMITER = ' -'
-    values = s.split(DELIMITER)
-    values = [v.strip() for v in values]
-    current = len(values)
-    if current < num:
-        values.extend([''] * (num - current))
-    return values
+# def _split(s, num):
+#     # 这里使用'- '的原因是为了解决:无网络条件下无法获取在线歌曲时长,
+#     # 导致写出格式存在问题,之前的_split无法解析下列异常情况
+#     # 'fuo://xiami/songs/1769834090	# Flower Dance - DJ OKAWARI -  -'
+#     # FIXME:修改写入部分的代码,而不是hack parse 过程
+#     DELIMITER = ' -'
+#     values = s.split(DELIMITER)
+#     values = [v.strip() for v in values]
+#     current = len(values)
+#     if current < num:
+#         values.extend([''] * (num - current))
+#     return values
+
+
+def _split(s: str, num: int) -> list:
+    class ParseState(Enum):
+        # 寻找下一个字段
+        find_next_filed = 0
+        # 解析字段
+        parse_normal_filed = 1
+        # 解析转义字段
+        parse_quoted_filed = 2
+        # 解析分隔符
+        parse_delimiter = 3
+
+    # filed1 - filed2 - filed3 - "filed4"
+    parse_state = ParseState.find_next_filed
+
+    fields = []
+    field = ""
+    # TODO:修复类似这样的解析"Flower Dance - DJ OKAWARI -  - "
+    # 特别注意这种形式"Flower Dance - DJ OKAWARI -  - 4:23"
+    # 中间字段的空白可能导致解析错位
+    s += '\n'
+    for ch in s:
+        if parse_state is ParseState.find_next_filed:
+            if ch == ' ' or ch == '\n':
+                continue
+            if ch == '"':
+                parse_state = ParseState.parse_quoted_filed
+            else:
+                parse_state = ParseState.parse_normal_filed
+                field += ch
+        elif parse_state is ParseState.parse_normal_filed:
+            if ch == '\n':
+                fields.append(field)
+                field = ""
+            elif ch == '-':
+                # TODO:去除末尾空格
+                fields.append(field)
+                field = ""
+                parse_state = ParseState.find_next_filed
+            else:
+                field += ch
+        elif parse_state is ParseState.parse_quoted_filed:
+            if ch == '"':
+                fields.append(field)
+                field = ""
+                parse_state = ParseState.parse_delimiter
+            else:
+                field += ch
+        elif parse_state is ParseState.parse_delimiter:
+            if ch == '-':
+                parse_state = ParseState.find_next_filed
+    if len(fields) != num:
+        current = len(fields)
+
+        logging.warning(
+            '_split("{}") expect to get {} fields,'.format(s, num, )
+            + 'but get {} fields:\n{}'.format(current, fields))
+
+        if current < num:
+            fields.extend([''] * (num - current))
+
+    return fields
 
 
 def parse_song_str(song_str):
@@ -133,7 +213,6 @@ def parse_line(line):
     parts = line.split('#', maxsplit=1)
     if len(parts) == 2:
         uri, model_str = parts
-        model_str = _model_unescape(model_str)
     else:
         uri, model_str = parts[0], ''
     ns_list = list(TYPE_NS_MAP.values())
@@ -183,8 +262,22 @@ def resolve(line, model=None):
                 method_name = 'resolve_' + path.replace('/', '_')
                 handler = getattr(model, method_name)
                 return handler()
-        raise ResolverNotFound('resolver-not-found for {}/{}'.format(str(model), path))
+        raise ResolverNotFound(
+            'resolver-not-found for {}/{}'.format(str(model), path))
     return model
+
+
+def do_reverse(fields: list) -> str:
+    length = len(fields)
+    if length == 0:
+        return ""
+    line = ""
+    for field in fields[0:-1]:
+        if not field:
+            field = ' '
+        line += _field_escape(field) + ' - '
+    line += fields[-1] if fields[-1] else ' '
+    return line
 
 
 def reverse(model, path='', as_line=False):
@@ -200,23 +293,27 @@ def reverse(model, path='', as_line=False):
     if as_line:
         if model.meta.model_type == ModelType.song:
             song = model
-            model_str = '{} - {} - {} - {}'.format(
-                _model_escape(song.title_display),
-                _model_escape(song.artists_name_display),
-                _model_escape(song.album_name_display),
-                _model_escape(song.duration_ms_display)
+            model_str = do_reverse(
+                [
+                    song.title_display,
+                    song.artists_name_display,
+                    song.album_name_display,
+                    song.duration_ms_display
+                ]
             )
         elif model.meta.model_type == ModelType.album:
             album = model
-            model_str = '{} - {}'.format(
-                _model_escape(album.name_display),
-                _model_escape(album.artists_name_display)
+            model_str = do_reverse(
+                [
+                    album.name_display,
+                    album.artists_name_display
+                ]
             )
 
         elif model.meta.model_type == ModelType.artist:
             artist = model
-            model_str = '{}'.format(
-                _model_escape(artist.name_display)
+            model_str = do_reverse(
+                [artist.name_display]
             )
         else:
             model_str = ''
