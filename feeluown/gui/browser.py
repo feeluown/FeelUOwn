5c3115a7c6beaafb1143944045749a861c1a9e6b
cosven <yinshaowen241@gmail.com>
2020-12-22 10:02:40 +0800

    gui: add page model

--- feeluown/gui/browser.py
+++ feeluown/gui/browser.py
@@ -8,7 +8,7 @@ from PyQt5.QtGui import QKeySequence
 
 from feeluown.utils import aio
 from feeluown.utils.router import Router, NotFound
-from feeluown.models.uri import resolve, reverse, ResolveFailed
+from feeluown.models.uri import resolve, reverse, ResolveFailed, parse_line
 
 logger = logging.getLogger(__name__)
 
@@ -45,18 +45,37 @@ class Browser:
         """跳转到 model 页面或者具体的地址
 
         必须提供 model 或者 path 其中一个参数，都提供时，以 model 为准。
+
+        Typical usage::
+
+            goto(model=model, path=path, query=xxx)
+            goto(uri=uri, query=xxx)
         """
-        if uri is not None:
-            warnings.warn('please use path instead of uri')
-            path = uri
         if query:
             qs = urlencode(query)
-            path = path + '?' + qs
-        self._goto(model, path)
-        if self._last_uri is not None and self._last_uri != self.current_uri:
-            self._back_stack.append(self._last_uri)
-        self._forward_stack.clear()
-        self.on_history_changed()
+        else:
+            qs = ''
+        try:
+            if model is not None:
+                if qs:
+                    path = (path or '') + '?' + qs
+                self._goto_model(model, path)
+            else:
+                # old code use goto(path=uri)
+                uri = uri or path
+                if qs:
+                    uri = uri + '?' + qs
+                if not uri.startswith('fuo://'):
+                    warnings.warn(f'browser uri should start with fuo://, {uri}')
+                    uri = f'fuo://{uri}'
+                self._goto_uri(uri)
+        except NotFound:
+            self._app.show_msg('-> {uri} ...failed', timeout=1)
+        else:
+            if self._last_uri is not None and self._last_uri != self.current_uri:
+                self._back_stack.append(self._last_uri)
+            self._forward_stack.clear()
+            self.on_history_changed()
 
     def back(self):
         try:
@@ -64,7 +83,7 @@ class Browser:
         except IndexError:
             logger.warning("Can't go back.")
         else:
-            self._goto(uri=uri)
+            self._goto_uri(uri=uri)
             self._forward_stack.append(self._last_uri)
             self.on_history_changed()
 
@@ -74,7 +93,7 @@ class Browser:
         except IndexError:
             logger.warning("Can't go forward.")
         else:
-            self._goto(uri=uri)
+            self._goto_uri(uri=uri)
             self._back_stack.append(self._last_uri)
             self.on_history_changed()
 
@@ -82,33 +101,41 @@ class Browser:
         """路由装饰器 (alpha)"""
         return self.router.route(rule)
 
-    def _goto(self, model=None, uri=None):
-        """真正的跳转逻辑"""
-        if model is None:
+    def _goto_uri(self, uri):
+        assert uri.startswith('fuo://')
+        # see if the uri match the two special cases
+        # fuo:///models/<provider>/<ns>/<identifier>
+        # fuo://<provider>/<ns>/<identifier>
+        if uri.startswith('fuo:///models/') or not uri.startswith('fuo:///'):
             try:
-                model = resolve(uri)
+                # FIXME: resolve is temporarily too magic
+                model_uri = 'fuo://' + uri[14:]
+                model, path = parse_line(model_uri)
+                model = resolve(reverse(model))
             except ResolveFailed:
                 model = None
-        else:
-            uri = reverse(model)
-        if not uri.startswith('fuo://'):
-            uri = 'fuo://' + uri
-        with self._app.create_action('-> {}'.format(uri)) as action:
-            if model is not None:
-                self._render_model(model)
+                logger.warning(f'invalid browser model uri:{uri}')
             else:
-                try:
-                    x = self.router.dispatch(uri, {'app': self._app})
-                    if inspect.iscoroutine(x):
-                        aio.create_task(x)
-                except NotFound:
-                    action.failed(f'{uri} not found.')
-                    return
-        self._last_uri = self.current_uri
-        if model is not None:
-            self.current_uri = reverse(model)
+                return self._goto_model(model, path)
         else:
-            self.current_uri = uri
+            self._goto(uri, {'app': self._app})
+
+    def _goto_model(self, model, path=''):
+        path = path or ''
+        base_uri = 'fuo:///models/' + reverse(model)[6:]
+        if path:
+            uri = f'{base_uri}/{path}'
+        else:
+            uri = base_uri
+        self._goto(uri, {'app': self._app, 'model': model})
+
+    def _goto(self, uri, ctx):
+        x = self.router.dispatch(uri, ctx)
+        if inspect.iscoroutine(x):
+            aio.create_task(x)
+
+        self._last_uri = self.current_uri
+        self.current_uri = uri
 
     @property
     def can_back(self):
@@ -121,10 +148,6 @@ class Browser:
     # --------------
     # UI Controllers
     # --------------
-
-    def _render_model(self, model):
-        """渲染 model 页面"""
-        self._app.ui.right_panel.show_model(model)
 
     def _render_coll(self, _, identifier):
         coll = self._app.coll_uimgr.get(int(identifier))
@@ -143,12 +166,12 @@ class Browser:
 
         1. bind routes with handler
         """
-        from feeluown.gui.pages import (
-            render_search,
-            render_player_playlist,
-        )
+        from feeluown.gui.pages.search import render as render_search
+        from feeluown.gui.pages.player_playlist import render as render_player_playlist
+        from feeluown.gui.pages.model import render as render_model
 
         urlpatterns = [
+            ('/models/<provider>/<ns>/<identifier>', render_model),
             ('/colls/<identifier>', self._render_coll),
             ('/search', render_search),
             ('/player_playlist', render_player_playlist),
