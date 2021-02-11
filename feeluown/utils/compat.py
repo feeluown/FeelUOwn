b644f1dfeffbc900e78a74db67129c64ba8cbc47
cosven <yinshaowen241@gmail.com>
2021-02-11 18:53:53 +0800

    *: try to fix #378

--- feeluown/utils/compat.py
+++ feeluown/utils/compat.py
@@ -1,20 +1,7 @@
 import sys
 
 
-# Choose proper qt-asyncio bridge(qumash/qasync/asyncqt) for
-# different python version.
-# https://github.com/feeluown/FeelUOwn/issues/346
-if sys.version_info >= (3, 6):
-    try:
-        from qasync import QEventLoop, QThreadExecutor
-    except ImportError:
-        raise ImportError("Please install python package 'qasync'") from None
-else:
-    # we use quamash instead of asyncqt since we have more experience in quamash
-    try:
-        from quamash import QEventLoop, QThreadExecutor
-    except ImportError:
-        raise ImportError("Please install python package 'quamash'") from None
+from qasync import QEventLoop, QThreadExecutor
 
 
 __all__ = (
