391aafe3a9425dca9e738c34e05ee7d945a63712
cosven <yinshaowen241@gmail.com>
2021-03-02 21:55:48 +0800

    WIP

--- feeluown/gui/widgets/provider.py
+++ feeluown/gui/widgets/provider.py
@@ -1,6 +1,9 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QAbstractListModel, QModelIndex, Qt, QSize, QRectF
+from PyQt5.QtGui import QPainter, QTextOption, QPalette, QBrush  # noqa
+from PyQt5.QtWidgets import QStyledItemDelegate, QListView
 
-from .textlist import TextlistModel, TextlistView
+from feeluown.gui.helpers import ItemViewNoScrollMixin, resize_font, ReaderFetchMoreMixin
+from .textlist import TextlistModel
 
 
 class ProvidersModel(TextlistModel):
@@ -29,11 +32,50 @@ class ProvidersModel(TextlistModel):
         return super().data(index, role)
 
 
-class ProvidersView(TextlistView):
-    def __init__(self, parent):
+class ProvidersDelegate(QStyledItemDelegate):
+    def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.setMinimumHeight(80)
+    def paint(self, painter, option, index):
+        painter.setRenderHint(QPainter.Antialiasing)
+        provider = index.data(Qt.UserRole)
+        ch = provider._name[0].capitalize()
+
+        painter.save()
+        text_color = option.palette.color(QPalette.Text)
+        if text_color.lightness() > 150:
+            non_text_color = text_color.darker(140)
+        else:
+            non_text_color = text_color.lighter(150)
+        non_text_color.setAlpha(100)
+        brush = QBrush(non_text_color)
+        painter.setBrush(brush)
+        painter.drawRoundedRect(option.rect, 24, 24)
+        painter.restore()
+
+        painter.save()
+        font = painter.font()
+        resize_font(font, +4)
+        text_option = QTextOption()
+        text_option.setAlignment(Qt.AlignCenter)
+        painter.drawText(QRectF(option.rect), ch, text_option)
+        painter.restore()
+
+    def sizeHint(self, option, index):
+        return QSize(48, 48)
+
+
+class ProvidersView(QListView):
+    def __init__(self, parent):
+        super().__init__()
+        QListView.__init__(self, parent=parent)
+
+        self.delegate = ProvidersDelegate(self)
+        self.setItemDelegate(self.delegate)
+        self.setViewMode(QListView.IconMode)
+        self.setResizeMode(QListView.Adjust)
+        self.setWrapping(True)
+
         self.clicked.connect(self._on_clicked)
 
     def _on_clicked(self, index):
