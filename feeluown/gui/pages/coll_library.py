4024177a3a8ad3a1e72ba21b36a3437bb4597f79
cosven <yinshaowen241@gmail.com>
2021-06-27 09:27:50 +0800

    use tabbar at search/coll_library pages

--- feeluown/gui/pages/coll_library.py
+++ feeluown/gui/pages/coll_library.py
@@ -1,70 +1,44 @@
 from feeluown.models import ModelType
 from feeluown.utils.reader import wrap
-from feeluown.gui.widgets.tabbar import Tab, TabBar
 from feeluown.gui.page_containers.table import Renderer
 
-from feeluown.gui.base_renderer import LibraryTabRendererMixin
+from feeluown.gui.base_renderer import TabBarRendererMixin
 
 
 async def render(req, **kwargs):
     app = req.ctx['app']
-    tab_index = int(req.query.get('tab_index', 0))
-
     ui = app.ui
+    tab_index = int(req.query.get('tab_index', 0))
     ui.right_panel.set_body(ui.right_panel.scrollarea)
-
-    tabbar = TabBar()
-    ui.toolbar.add_stacked_widget(tabbar)
-    ui.toolbar.set_top_stacked_widget(tabbar)
-    mapping = [('歌曲', Tab.songs),
-                ('专辑', Tab.albums),
-                ('歌手', Tab.artists),
-                ('歌单', Tab.playlists),
-                ('视频', Tab.videos)]
-
-    target_tab_id = None
-    for i, (tab_name, tab_id) in enumerate(mapping):
-        tabbar.addTab(tab_name)
-        if i == tab_index:
-            target_tab_id = tab_id
-
-    tabbar.setCurrentIndex(tab_index)
-    tabbar.tabBarClicked.connect(
-        lambda tab_index: app.browser.goto(page='/colls/library',
-                                           query={'tab_index': tab_index}))
-
     table_container = ui.right_panel.table_container
-    renderer = LibraryRenderer(target_tab_id)
+    renderer = LibraryRenderer(tab_index)
     await table_container.set_renderer(renderer)
 
 
-class LibraryRenderer(Renderer, LibraryTabRendererMixin):
-    def __init__(self, tab_id):
-        self.tab_id = tab_id
+class LibraryRenderer(Renderer, TabBarRendererMixin):
+
+    def __init__(self, tab_index):
+        self.tab_index = tab_index
+        self.tabs = [
+            ('歌曲', ModelType.song, self.show_songs),
+            ('专辑', ModelType.album, self.show_albums),
+            ('歌手', ModelType.artist, self.show_artists),
+            ('歌单', ModelType.playlist, self.show_playlists),
+            ('视频', ModelType.video, self.show_videos)
+        ]
 
     async def render(self):
-        self.render_tabbar()
-
         self.meta_widget.show()
         self.meta_widget.title = '音乐库'
+        self.render_tab_bar()
 
         coll = self._app.coll_uimgr.get_coll_library()
-        mtype = self.get_tabid_mtype_mapping()[self.tab_id]
+        _, mtype, show_handler = self.tabs[self.tab_index]
         models = [model for model in coll.models
                   if model.meta.model_type == mtype]
         reader = wrap(models)
-        show_handler = self.get_tabid_handler_mapping()[self.tab_id]
         show_handler(reader)
 
-    def show_by_tab_id(self, tab_id):
-        query = {'tab_id': tab_id.value}
-        self._app.browser.goto(page='/colls/library', query=query)
-
-    def get_tabid_mtype_mapping(self):
-        return {
-            Tab.songs: ModelType.song,
-            Tab.albums: ModelType.album,
-            Tab.artists: ModelType.artist,
-            Tab.playlists: ModelType.playlist,
-            Tab.videos: ModelType.video,
-        }
+    def render_by_tab_index(self, tab_index):
+        self._app.browser.goto(page='/colls/library',
+                               query={'tab_index': tab_index})
