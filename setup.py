3a07e0632cd2ee08c3a5b63881202b8e6e1621fc
cosven <yinshaowen241@gmail.com>
2021-12-15 21:09:15 +0800

    update setup.py description

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
         'Environment :: X11 Applications :: Qt',
         "Topic :: Multimedia :: Sound/Audio",
