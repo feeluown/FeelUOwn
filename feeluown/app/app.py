fef5e7e1d10e8e645b6258b01e89312ceb60e0cb
cosven <yinshaowen241@gmail.com>
2021-12-27 19:52:25 +0800

    add comments

--- feeluown/app/app.py
+++ feeluown/app/app.py
@@ -22,6 +22,13 @@ logger = logging.getLogger(__name__)
 
 
 class AppMode(IntFlag):
+    """
+    When server mode is on, rpc server and pubsub server are both started.
+    When gui mode is on, GUI window is created and shown. Server and gui mode
+    can be both on. When cli mode is on, server and gui modes are off (currently).
+
+    cli mode is *experimental* feature temporarily.
+    """
     server = 0x0001  # 开启 Server
     gui = 0x0010     # 显示 GUI
     cli = 0x0100     # 命令行模式
