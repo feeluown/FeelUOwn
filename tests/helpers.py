b7c4c2793f189b5c28a9f21ebcb0df5252cec0a2
cosven <yinshaowen241@gmail.com>
2022-01-15 15:17:35 +0800

    run more qt tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,4 +4,6 @@ import os
 is_travis_env = os.environ.get('TEST_ENV') == 'travis'
 
 cannot_play_audio = is_travis_env
-cannot_run_qt_test = is_travis_env
+
+# By set QT_QPA_PLATFORM=offscreen, qt widgets can run on various CI.
+cannot_run_qt_test = False
