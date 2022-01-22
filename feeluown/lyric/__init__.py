13046f13c05af87faf0a688c630c7d83b4f01c0c
cosven <cosven@users.noreply.github.com>
2022-01-22 16:06:59 +0800

    *: move feeluown.lyric module into feeluown.player package (#545)

--- feeluown/lyric/__init__.py
+++
@@ -1,8 +0,0 @@
-from .lyric import parse
-from .live_lyric import LiveLyric
-
-
-__all__ = (
-    'LiveLyric',
-    'parse',  # versionadded: 3.7.8
-)
--- feeluown/player/lyric.py
+++ feeluown/player/lyric.py
@@ -1,12 +1,38 @@
+import re
 import logging
+from typing import Dict, List
 
 from feeluown.utils import aio
 from feeluown.utils.dispatch import Signal
 from feeluown.utils.utils import find_previous
 
-from .lyric import parse
-
 logger = logging.getLogger(__name__)
+
+
+def parse_lyric_text(content: str) -> Dict[int, str]:
+    """
+    Reference: https://github.com/osdlyrics/osdlyrics/blob/master/python/lrc.py
+
+    >>> parse_lyric_text("[00:00.00] 作曲 : 周杰伦\\n[00:01.00] 作词 : 周杰伦\\n")
+    {0: ' 作曲 : 周杰伦', 1000: ' 作词 : 周杰伦'}
+    """
+    ms_sentence_map = dict()
+    sentence_pattern = re.compile(r'\[(\d+(:\d+){0,2}(\.\d+)?)\]')
+    lines = content.splitlines()
+    for line in lines:
+        m = sentence_pattern.search(line, 0)
+        if m:
+            time_str = m.group(1)
+            mileseconds = 0
+            unit = 1000
+            t_seq = time_str.split(':')
+            t_seq.reverse()
+            for num in t_seq:
+                mileseconds += int(float(num) * unit)
+                unit *= 60
+            sentence = line[m.end():]
+            ms_sentence_map[mileseconds] = sentence
+    return ms_sentence_map
 
 
 class LiveLyric(object):
@@ -22,12 +48,16 @@ class LiveLyric(object):
         player.position_change.connect(live_lyric.on_position_changed)
     """
     def __init__(self, app):
+        """
+
+        :type app: feeluown.app.App
+        """
         self._app = app
         self.sentence_changed = Signal(str)
 
         self._lyric = None
-        self._pos_s_map = {}  # position sentence map
-        self._pos_list = []  # position list
+        self._pos_s_map: Dict[int, str] = {}  # position sentence map
+        self._pos_list: List[int] = []  # position list
         self._pos = None
 
         self._current_sentence = ''
@@ -76,7 +106,7 @@ class LiveLyric(object):
             self._pos_s_map = {}
         else:
             self._lyric = lyric.content
-            self._pos_s_map = parse(self._lyric)
+            self._pos_s_map = parse_lyric_text(self._lyric)
             self._pos_list = sorted(list(self._pos_s_map.keys()))
             self._pos = None
             self.current_sentence = ''
