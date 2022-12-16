ad2b911a87b3cd066bd0b52207d06ce1624591e5
cosven <yinshaowen241@gmail.com>
2022-12-16 22:59:58 +0800

    library: fix get model

--- feeluown/excs.py
+++ feeluown/excs.py
@@ -67,6 +67,13 @@ class ModelNotFound(ResourceNotFound):
     """
 
 
+class ModelCannotUpgrade(LibraryException):
+    """Model cannot be upgraded
+
+    .. versionadded:: 3.8.9
+    """
+
+
 class NotSupported(LibraryException):
     """Provider does not support the operation
     """
