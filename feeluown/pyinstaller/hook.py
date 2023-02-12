1a29c30ec7f60ee88c4d3a7542f033d510c75b52
cosven <yinshaowen241@gmail.com>
2023-02-12 20:10:15 +0800

    fix lint

--- feeluown/pyinstaller/hook.py
+++ feeluown/pyinstaller/hook.py
@@ -3,5 +3,5 @@ import pathlib
 
 def get_hook_dirs():
     root = pathlib.Path(__file__).resolve().parent.parent.parent
-    hooks_dir = str( root / 'pyinstaller_hooks')
+    hooks_dir = str(root / 'pyinstaller_hooks')
     return [hooks_dir]
