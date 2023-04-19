1ebac391e262345da0632eed6885fbd62eb4687c
cosven <yinshaowen241@gmail.com>
2023-04-19 20:10:56 +0800

    bundle: support macos

--- pyinstaller_hooks/hook-feeluown.py
+++ pyinstaller_hooks/hook-feeluown.py
@@ -1,8 +1,17 @@
 import os
+import sys
 import ctypes
 from PyInstaller.utils.hooks import collect_data_files, collect_entry_point
 
 
+assets_files = [
+    '**/*.png',
+    '**/*.qss',
+    '**/*.svg',
+    '**/*.ico',
+    '**/*.icns',
+    '**/*.colors'
+]
 datas, hiddenimports = collect_entry_point('fuo.plugins_v1')
 
 # aionowplaying is conditionally imported.
@@ -10,12 +19,16 @@ if os.name == 'nt':
     hiddenimports.append('aionowplaying')
     # pyinstaller can't detect 'aionowplaying.interface.windows' is imported.
     hiddenimports.append('aionowplaying.interface.windows')
+if sys.platform == 'darwin':
+    hiddenimports.append('aionowplaying.interface.macos')
+    hiddenimports.append('feeluown.aionowplaying.macos')
 
 # Collect feeluown's resource files, like icons, qss files, etc.
 # Collect plugins' resource files.
-datas += collect_data_files('feeluown')
+datas += collect_data_files('feeluown', includes=assets_files)
 for pkg in hiddenimports:
-    datas += collect_data_files(pkg)
+    datas += collect_data_files(pkg, includes=assets_files)
+
 
 # Collect mpv dynamic-link library.
 if os.name == 'nt':
