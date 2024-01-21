c63543661e98f4ec84d24dd746b642954395ef46
Shaowen Yin <cosven@users.noreply.github.com>
2024-01-21 12:21:24 +0800

    *: show thanks to contributors (#777)

--- feeluown/gui/tips.py
+++ feeluown/gui/tips.py
@@ -3,6 +3,46 @@ import random
 from feeluown.utils import aio
 from feeluown.app import App
 
+# (nickname, name), no order.
+# Write your Chinese(or other languages) name if you want.
+Contributors = [
+    ('cosven', 'Shaowen Yin'),
+    ('felixonmars', ''),
+    ('PhotonQuantum', ''),
+    ('wuliaotc', ''),
+    ('cyliuu', ''),
+    ('light4', ''),
+    ('hjlarry', ''),
+    ('sbwtw', ''),
+    ('oryxfea', ''),
+    ('poly000', ''),
+    ('BruceZhang1993', ''),
+    ('cposture', ''),
+    ('helinb', ''),
+    ('Yexiaoxing', ''),
+    ('albertofwb', ''),
+    ('catsout', ''),
+    ('Torxed', ''),
+    ('RealXuChe', ''),
+    ('rapiz1', ''),
+    ('Heersin', ''),
+    ('chen-chao', ''),
+    ('keter42', ''),
+    ('timgates42', ''),
+    ('junhan-z', ''),
+    ('CareF', ''),
+    ('berberman', ''),
+    ('SaneBow', ''),
+    ('wsyxbcl', ''),
+    ('Thearas', ''),
+    ('reaink', ''),
+    ('wenLiangcan', ''),
+    ('leedagee', ''),
+    ('hanchanli', ''),
+    ('xssss1', ''),
+    ('williamherry', ''),
+]
+
 
 class TipsManager:
     """在合适的时候展示一些使用 Tip"""
@@ -22,7 +62,17 @@ class TipsManager:
         self._app.started.connect(self.show_random_after_5s)
 
     def show_random(self):
-        self._app.show_msg(random.choice(self.tips), timeout=2500)
+        if random.randint(0, 10) < 5:
+            self._app.show_msg(random.choice(self.tips), timeout=2500)
+        else:
+            contributor = random.choice(Contributors)
+            nickname, name = contributor
+            if name:
+                user = f'{name} (@{nickname})'
+            else:
+                user = f'@{nickname}'
+            msg = f'感谢 {user} 的贡献 :)'
+            self._app.show_msg(msg, timeout=2500)
 
     def show_random_after_5s(self, *_):
         async def task():
