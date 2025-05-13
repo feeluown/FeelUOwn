52fea91df84ede0a255ea01483c8588166c373a6
cosven <yinshaowen241@gmail.com>
2025-05-13 23:12:06 +0800

    # Diffs: Added feeluown/gui/pages/my_dislike.py feat: add my dislike page

---
+++ feeluown/gui/pages/my_dislike.py
@@ -0,0 +1,74 @@
+from feeluown.app.gui_app import GuiApp
+from feeluown.library import ModelType
+from feeluown.utils.aio import run_fn
+from feeluown.gui.page_containers.table import Renderer
+from feeluown.gui.base_renderer import TabBarRendererMixin
+from feeluown.library import (
+    SupportsCurrentUserDislikeSongsReader,
+    SupportsCurrentUserDislikeRemoveSong,
+)
+from feeluown.utils.reader import create_reader
+from .template import render_error_message
+
+
+async def render(req, **kwargs):
+    app: GuiApp = req.ctx['app']
+    ui = app.ui
+    tab_index = int(req.query.get('tab_index', 0))
+    pvd_ui = app.current_pvd_ui_mgr.get()
+    if pvd_ui is None:
+        return await render_error_message(app, '当前资源提供方未知，无法浏览该页面')
+
+    provider = pvd_ui.provider
+    if isinstance(provider, SupportsCurrentUserDislikeSongsReader):
+        reader = await run_fn(provider.current_user_dislike_create_songs_rd)
+        songs = reader.readall()
+        # TODO: add a widget to show the songs
+
+
+    # ui.right_panel.set_body(ui.right_panel.scrollarea)
+    # table_container = ui.right_panel.table_container
+    # renderer = DislikeRenderer(app, tab_index, pvd_ui.provider)
+    # await table_container.set_renderer(renderer)
+
+
+class DislikeRenderer(Renderer, TabBarRendererMixin):
+    def __init__(self, app, tab_index, provider):
+        self._app = app
+        self._provider = provider
+        self.tab_index = tab_index
+        self.tabs = self.default_tabs()[:1]
+
+    async def render(self):
+        self.meta_widget.show()
+        self.meta_widget.title = '音乐黑名单'
+        self.render_tab_bar()
+        await self.render_models()
+
+    async def render_models(self):
+        _, mtype, show_handler = self.tabs[self.tab_index]
+        err = ''
+        reader = create_reader([])
+        if mtype is ModelType.song:
+            if isinstance(self._provider, SupportsCurrentUserDislikeSongsReader):
+                reader = await run_fn(self._provider.current_user_dislike_create_songs_rd)
+            else:
+                err = '不喜欢的歌曲'
+            if isinstance(self._provider, SupportsCurrentUserDislikeRemoveSong):
+                self.songs_table.remove_song_func = \
+                    self._provider.current_user_dislike_remove_song
+        else:
+            err = '未知类型资源'
+
+        if err:
+            return await render_error_message(
+                self._app,
+                f'当前资源提供方（{self._provider.name}）不支持获取 {err}'
+            )
+        else:
+            show_handler(reader)
+        # Do not show playall button.
+        self.toolbar.hide()
+
+    def render_by_tab_index(self, tab_index):
+        self._app.browser.goto(page='/my_dislike', query={'tab_index': tab_index})
