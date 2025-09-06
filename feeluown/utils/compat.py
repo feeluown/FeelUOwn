0100e01a6c2c1ab71138b39ddb08ae526a61a5b6
Shaowen Yin <yinshaowen241@gmail.com>
2025-09-06 15:26:44 +0800

    qt: fix #950 (#951)

--- feeluown/utils/compat.py
+++ feeluown/utils/compat.py
@@ -1,4 +1,17 @@
-from qasync import QEventLoop, QThreadExecutor, DefaultQEventLoopPolicy
+import asyncio
+import sys
+
+from qasync import QEventLoop, QThreadExecutor
+from PyQt5.QtWidgets import QApplication
+
+try:
+    from qasync import DefaultQEventLoopPolicy
+except ImportError:
+    # From qasync>=0.28, the class DefaultQEventLoopPolicy is removed
+    # when python version >= 3.12
+    class DefaultQEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
+        def new_event_loop(self):
+            return QEventLoop(QApplication.instance() or QApplication(sys.argv))
 
 
 __all__ = (
