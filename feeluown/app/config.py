551abffd3cd3580d5cfabf23eeaad29cc0151c64
莯凛 <mokurin000@gmail.com>
2026-01-25 21:42:32 +0800

    style: format argparser.py, config.py with ruff

--- feeluown/app/config.py
+++ feeluown/app/config.py
@@ -10,47 +10,52 @@ from feeluown.config import Config
 
 def create_config() -> Config:
     config = Config()
-    config.deffield('DEBUG', type_=bool, desc='是否为调试模式')
-    config.deffield('VERBOSE', default=0, type_=int, desc='日志详细程度')
-    config.deffield('RPC_PORT', default=23333, type_=int, desc='RPC 端口')
-    config.deffield('PUBSUB_PORT', default=23334, type_=int, desc='PUBSUB 端口')
-    config.deffield('WEB_PORT', default=23332, type_=int, desc='WEB 服务端口')
-    config.deffield('ENABLE_WEB_SERVER', default=False, type_=bool)
-    config.deffield('MODE', default=0x0000, desc='CLI or GUI 模式')
-    config.deffield('THEME', default='auto', desc='auto/light/dark')
-    config.deffield('ENABLE_NEW_HOMEPAGE', default=True, type_=bool)
+    config.deffield("DEBUG", type_=bool, desc="是否为调试模式")
+    config.deffield("VERBOSE", default=0, type_=int, desc="日志详细程度")
+    config.deffield("RPC_PORT", default=23333, type_=int, desc="RPC 端口")
+    config.deffield("PUBSUB_PORT", default=23334, type_=int, desc="PUBSUB 端口")
+    config.deffield("WEB_PORT", default=23332, type_=int, desc="WEB 服务端口")
+    config.deffield("ENABLE_WEB_SERVER", default=False, type_=bool)
+    config.deffield("MODE", default=0x0000, desc="CLI or GUI 模式")
+    config.deffield("THEME", default="auto", desc="auto/light/dark")
+    config.deffield("ENABLE_NEW_HOMEPAGE", default=True, type_=bool)
     config.deffield(
-        'NEW_HOMEPAGE_SETTINGS',
+        "NEW_HOMEPAGE_SETTINGS",
         type_=dict,
         default={
-            'contents': [
-                {'name': 'RecListDailySongs', 'provider': 'netease'},
+            "contents": [
+                {"name": "RecListDailySongs", "provider": "netease"},
                 # {'name': 'RecACollectionOfSongs', 'provider': 'qqmusic'},
-                {'name': 'RecListDailyPlaylists', 'provider': 'qqmusic'},
-                {'name': 'RecACollectionOfVideos', 'provider': 'bilibili'},
+                {"name": "RecListDailyPlaylists", "provider": "qqmusic"},
+                {"name": "RecACollectionOfVideos", "provider": "bilibili"},
             ]
         },
-        desc='主页配置'
+        desc="主页配置",
     )
-    config.deffield('MPV_AUDIO_DEVICE', default='auto', desc='MPV 播放设备')
-    config.deffield('COLLECTIONS_DIR', desc='本地收藏所在目录')
+    config.deffield("MPV_AUDIO_DEVICE", default="auto", desc="MPV 播放设备")
+    config.deffield("COLLECTIONS_DIR", desc="本地收藏所在目录")
     config.deffield(
-        'FORCE_MAC_HOTKEY',
-        desc='强制开启 macOS 全局快捷键功能',
-        warn='Will be remove in version 3.0'
+        "FORCE_MAC_HOTKEY",
+        desc="强制开启 macOS 全局快捷键功能",
+        warn="Will be remove in version 3.0",
     )
-    config.deffield('LOG_TO_FILE', desc='将日志输出到文件中')
-    config.deffield('AUDIO_SELECT_POLICY', default='hq<>')
-    config.deffield('VIDEO_SELECT_POLICY', default='hd<>')
-    config.deffield('ALLOW_LAN_CONNECT', type_=bool, default=False, desc='是否可以从局域网连接服务器')
-    config.deffield('PROVIDERS_STANDBY', type_=list, default=None, desc='')
+    config.deffield("LOG_TO_FILE", desc="将日志输出到文件中")
+    config.deffield("AUDIO_SELECT_POLICY", default="hq<>")
+    config.deffield("VIDEO_SELECT_POLICY", default="hd<>")
+    config.deffield(
+        "ALLOW_LAN_CONNECT",
+        type_=bool,
+        default=False,
+        desc="是否可以从局域网连接服务器",
+    )
+    config.deffield("PROVIDERS_STANDBY", type_=list, default=None, desc="")
 
     # YTDL related fields are deprecated since v4.1.9. Disable them by default.
     config.deffield(
-        'ENABLE_YTDL_AS_MEDIA_PROVIDER',
+        "ENABLE_YTDL_AS_MEDIA_PROVIDER",
         type_=bool,
         default=False,
-        desc='(Deprecated) YTDL 作为备用资源'
+        desc="(Deprecated) YTDL 作为备用资源",
     )
     # For example::
     #    [
@@ -63,42 +68,48 @@ def create_config() -> Config:
     #            },
     #        },
     #    ]
-    config.deffield('YTDL_RULES', type_=list, default=None, desc='(Deprecated)')
+    config.deffield("YTDL_RULES", type_=list, default=None, desc="(Deprecated)")
 
     # TODO(cosven): maybe
     # 1. when it is set to 2, find standby from other providers first.
     # 2. when it is set to 3, play it's MV model instead of using MV's media.
-    config.deffield('ENABLE_MV_AS_STANDBY', type_=int, default=1, desc='MV 作为备用资源')
-    config.deffield('ENABLE_TRAY', type_=bool, default=True, desc='启用系统托盘')
     config.deffield(
-        'NOTIFY_ON_TRACK_CHANGED', type_=bool, default=False, desc='切换歌曲时显示桌面通知'
+        "ENABLE_MV_AS_STANDBY", type_=int, default=1, desc="MV 作为备用资源"
     )
-    config.deffield('NOTIFY_DURATION', type_=int, default=3000, desc='桌面通知保留时长(ms)')
-    config.deffield('PLAYBACK_CROSSFADE', type_=bool, default=False, desc='播放暂停淡入淡出')
+    config.deffield("ENABLE_TRAY", type_=bool, default=True, desc="启用系统托盘")
     config.deffield(
-        'PLAYBACK_CROSSFADE_DURATION', type_=int, default=500, desc='淡入淡出持续时间'
+        "NOTIFY_ON_TRACK_CHANGED",
+        type_=bool,
+        default=False,
+        desc="切换歌曲时显示桌面通知",
     )
     config.deffield(
-        'OPENAI_API_BASEURL',
-        type_=str,
-        default='',
-        desc='OpenAI API base url'
+        "NOTIFY_DURATION", type_=int, default=3000, desc="桌面通知保留时长(ms)"
     )
-    config.deffield('OPENAI_API_KEY', type_=str, default='', desc='OpenAI API key')
-    config.deffield('OPENAI_MODEL', type_=str, default='', desc='OpenAI model name')
+    config.deffield(
+        "PLAYBACK_CROSSFADE", type_=bool, default=False, desc="播放暂停淡入淡出"
+    )
+    config.deffield(
+        "PLAYBACK_CROSSFADE_DURATION", type_=int, default=500, desc="淡入淡出持续时间"
+    )
+    config.deffield(
+        "OPENAI_API_BASEURL", type_=str, default="", desc="OpenAI API base url"
+    )
+    config.deffield("OPENAI_API_KEY", type_=str, default="", desc="OpenAI API key")
+    config.deffield("OPENAI_MODEL", type_=str, default="", desc="OpenAI model name")
     # Not sure if AI_STANDBY_MATCHER may be activated unexpectedly.
     # And it may burn some money, let user enable it manually.
-    config.deffield('ENABLE_AI_STANDBY_MATCHER', type_=bool, default=False, desc='')
+    config.deffield("ENABLE_AI_STANDBY_MATCHER", type_=bool, default=False, desc="")
     config.deffield(
-        'AI_RADIO_PROMPT',
+        "AI_RADIO_PROMPT",
         type_=str,
-        default='你是一个音乐播放器智能助手。',
-        desc='AI 电台功能的提示词'
+        default="你是一个音乐播放器智能助手。",
+        desc="AI 电台功能的提示词",
     )
     config.deffield(
-        'ENABLE_REPLACE_PLAYLIST_ON_DBLCLICK',
+        "ENABLE_REPLACE_PLAYLIST_ON_DBLCLICK",
         type_=bool,
         default=True,
-        desc='双击歌曲时，用歌曲所在列表替换当前播放列表'
+        desc="双击歌曲时，用歌曲所在列表替换当前播放列表",
     )
     return config
--- feeluown/argparser.py
+++ feeluown/argparser.py
@@ -6,21 +6,21 @@ from functools import partial
 def create_fmt_parser():
     fmt_parser = argparse.ArgumentParser(add_help=False)
     fmt_parser.add_argument(
-        '--format',
+        "--format",
         help="change command output format (default: plain)",
-        choices=['plain', 'json'],
+        choices=["plain", "json"],
     )
     fmt_parser.add_argument(
-        '--json',
-        action='store_true',
+        "--json",
+        action="store_true",
         default=False,
         help="change command output format to json",
     )
     fmt_parser.add_argument(
-        '--plain',
-        action='store_true',
+        "--plain",
+        action="store_true",
         default=False,
-        help="change command output format to plan"
+        help="change command output format to plan",
     )
     return fmt_parser
 
@@ -34,111 +34,108 @@ def add_common_cmds(subparsers: argparse._SubParsersAction):
     # Create parsers.
     #
     play_parser = add_parser(
-        'play',
-        description=textwrap.dedent('''\
+        "play",
+        description=textwrap.dedent("""\
         Example:
             - fuo play fuo://netease/songs/3027393
             - fuo play "in the end"
             - fuo play 稻香-周杰伦
-        '''),
+        """),
         formatter_class=argparse.RawTextHelpFormatter,
     )
     show_parser = add_parser(
-        'show',
-        help='显示资源详细信息',
+        "show",
+        help="显示资源详细信息",
     )
     search_parser = add_parser(
-        'search',
-        description=textwrap.dedent('''\
+        "search",
+        description=textwrap.dedent("""\
         Example:
             - fuo search hero
             - fuo search 李宗盛 source=xiami,type=artist
             - fuo search 李宗盛 [source=xiami,type=artist]
             - fuo search lizongsheng "source='xiami,qq',type=artist"
             - fuo search 李宗盛 "[source='xiami,qq',type=artist]"
-        '''),
+        """),
         formatter_class=argparse.RawTextHelpFormatter,
         parents=parents,
     )
-    remove_parser = add_parser('remove', help='从播放列表移除歌曲')
-    add_parser_ = add_parser('add', help='添加歌曲到播放列表')
-    exec_parser = add_parser('exec')
-    jsonrpc_parser = add_parser('jsonrpc')
-    add_parser('pause', help='暂停播放')
-    add_parser('resume', help='回复播放')
-    add_parser('toggle', help='')
-    add_parser('stop')
-    add_parser('next')
-    add_parser('previous')
-    add_parser('list')
-    add_parser('clear')
-    add_parser('status')
+    remove_parser = add_parser("remove", help="从播放列表移除歌曲")
+    add_parser_ = add_parser("add", help="添加歌曲到播放列表")
+    exec_parser = add_parser("exec")
+    jsonrpc_parser = add_parser("jsonrpc")
+    add_parser("pause", help="暂停播放")
+    add_parser("resume", help="回复播放")
+    add_parser("toggle", help="")
+    add_parser("stop")
+    add_parser("next")
+    add_parser("previous")
+    add_parser("list")
+    add_parser("clear")
+    add_parser("status")
 
     # Initialize parsers.
     #
-    play_parser.add_argument('uri')
-    show_parser.add_argument('uri')
-    remove_parser.add_argument('uri')
-    add_parser_.add_argument('uri', nargs='?')
-    search_parser.add_argument('keyword', help='搜索关键字')
-    search_parser.add_argument('-s', '--source', action='append')
+    play_parser.add_argument("uri")
+    show_parser.add_argument("uri")
+    remove_parser.add_argument("uri")
+    add_parser_.add_argument("uri", nargs="?")
+    search_parser.add_argument("keyword", help="搜索关键字")
+    search_parser.add_argument("-s", "--source", action="append")
     search_parser.add_argument(
-        '--type',
-        action='append',
+        "--type",
+        action="append",
         type=str,
-        choices=['song', 'album', 'artist', 'video', 'playlist']
+        choices=["song", "album", "artist", "video", "playlist"],
     )
-    exec_parser.add_argument('code', nargs='?', help='Python 代码')
-    jsonrpc_parser.add_argument('body', nargs='?', help='JSON-RPC 请求体')
+    exec_parser.add_argument("code", nargs="?", help="Python 代码")
+    jsonrpc_parser.add_argument("body", nargs="?", help="JSON-RPC 请求体")
 
 
 def add_cli_cmds(subparsers: argparse._SubParsersAction):
     # Generate desktop file/icon.
-    subparsers.add_parser(
-        'genicon',
-        description='generate desktop icon'
-    )
+    subparsers.add_parser("genicon", description="generate desktop icon")
 
 
 def add_server_cmds(subparsers: argparse._SubParsersAction, include_pubsub=True):
     fmt_parser = create_fmt_parser()
     subparsers.add_parser(
-        'quit',
+        "quit",
         parents=[fmt_parser],
     )
     help_parser = subparsers.add_parser(
-        'help',
+        "help",
         parents=[fmt_parser],
     )
-    help_parser.add_argument('cmdname')
+    help_parser.add_argument("cmdname")
     # The default value of these values should be None.
     # See set handler for more details.
     set_parser = subparsers.add_parser(
-        'set',
-        help='设置会话参数',
+        "set",
+        help="设置会话参数",
         parents=[fmt_parser],
     )
     set_parser.add_argument(
-        '--rpc-version',
+        "--rpc-version",
         type=str,
-        choices=['1.0', '2.0'],
+        choices=["1.0", "2.0"],
     )
     set_parser.add_argument(
-        '--pubsub-version',
+        "--pubsub-version",
         type=str,
-        choices=['1.0', '2.0'],
+        choices=["1.0", "2.0"],
     )
 
     if include_pubsub is True:
         sub_parser = subparsers.add_parser(
-            'sub',
-            help='订阅主题消息',
+            "sub",
+            help="订阅主题消息",
             parents=[fmt_parser],
         )
         sub_parser.add_argument(
-            'topics',
-            nargs='?',
-            help='主题名字',
+            "topics",
+            nargs="?",
+            help="主题名字",
         )
 
 
@@ -148,39 +145,39 @@ def _create_cli_parser() -> argparse.ArgumentParser:
 
     parser = argparse.ArgumentParser(
         description=textwrap.dedent(
-            '''\
+            """\
         FeelUOwn - modern music player (daemon).
 
         Example:
             - fuo                        # start fuo server
             - fuo status                 # lookup server status
             - fuo play 晴天-周杰伦       # search and play
-        '''
+        """
         ),
         formatter_class=argparse.RawTextHelpFormatter,
-        prog='feeluown'
+        prog="feeluown",
     )
 
     parser.add_argument(
-        '-V',
-        '--version',
-        action='version',
-        version='%(prog)s {}'.format(feeluown_version)
+        "-V",
+        "--version",
+        action="version",
+        version="%(prog)s {}".format(feeluown_version),
     )
     parser.add_argument(
-        '-ns', '--no-server', action='store_true', default=False, help='不运行 server'
+        "-ns", "--no-server", action="store_true", default=False, help="不运行 server"
     )
     parser.add_argument(
-        '-nw', '--no-window', action='store_true', default=False, help='不显示 GUI'
+        "-nw", "--no-window", action="store_true", default=False, help="不显示 GUI"
     )
 
     # options about log
     parser.add_argument(
-        '-d', '--debug', action='store_true', default=False, help='开启调试模式'
+        "-d", "--debug", action="store_true", default=False, help="开启调试模式"
     )
-    parser.add_argument('-v', '--verbose', action='count', help='输出详细的日志')
+    parser.add_argument("-v", "--verbose", action="count", help="输出详细的日志")
     parser.add_argument(
-        '--log-to-file', action='store_true', default=False, help='将日志打到文件中'
+        "--log-to-file", action="store_true", default=False, help="将日志打到文件中"
     )
 
     # XXX: Not sure if it's possible to add a regex-based option?
@@ -189,14 +186,14 @@ def _create_cli_parser() -> argparse.ArgumentParser:
 
     # TODO: Need to include in the documentation
     # how to view the available playback devices.
-    parser.add_argument('--mpv-audio-device', help='（高级选项）指定播放设备')
+    parser.add_argument("--mpv-audio-device", help="（高级选项）指定播放设备")
     return parser
 
 
 def create_cli_parser() -> argparse.ArgumentParser:
     parser = _create_cli_parser()
     subparsers = parser.add_subparsers(
-        dest='cmd',
+        dest="cmd",
     )
 
     add_common_cmds(subparsers)
