335db909df1ba48543e54d5fe05d4d09e6747cc0
Shaowen Yin <yinshaowen241@gmail.com>
2026-01-27 10:22:22 +0800

    tests: fix test_table_meta (#996)

--- tests/gui/widgets/test_meta.py
+++ tests/gui/widgets/test_meta.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pytest
+from PyQt6.QtWidgets import QWidget
 
 from tests.helpers import is_travis_env
 from feeluown.gui.widgets.meta import TableMetaWidget
@@ -10,8 +11,9 @@ from feeluown.gui.widgets.meta import TableMetaWidget
 # example: https://github.com/pytest-dev/pytest-qt/blob/master/.travis.yml
 @pytest.mark.skipif(is_travis_env, reason="travis env has no display")
 def test_table_meta(qtbot, app_mock):
-    widget = TableMetaWidget(app_mock)
-    qtbot.addWidget(widget)
+    p_widget = QWidget()
+    widget = TableMetaWidget(app_mock, p_widget)
+    qtbot.addWidget(p_widget)
     widget.title = '我喜欢的音乐'
     widget.subtitle = '嘿嘿'
     widget.creator = 'cosven'
