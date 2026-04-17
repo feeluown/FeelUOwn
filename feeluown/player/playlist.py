8e1194a775c2c44402094c12366bc0825b37d97f
Zihao <Louis20120127@gmail.com>
2026-04-17 13:18:38 +1000

    fix: single song loop no redownloads (#1024)

--- feeluown/player/playlist.py
+++ feeluown/player/playlist.py
@@ -449,6 +449,7 @@ class Playlist:
             self._leave_shuffle_mode()
 
         self._playback_mode = playback_mode
+        self._app.player.set_infinite_loop(playback_mode is PlaybackMode.one_loop)
         self.playback_mode_changed.emit(self.playback_mode)
 
     def _enter_shuffle_mode(self):
@@ -594,11 +595,7 @@ class Playlist:
 
     def _on_media_finished(self):
         # Play next model when current media is finished.
-        if self.playback_mode == PlaybackMode.one_loop:
-            with self._queue_lock:
-                return self.set_existing_song_as_current_song(self.current_song)
-        else:
-            self.next()
+        self.next()
 
     def _on_song_changed(self, song):
         self._app.task_mgr.run_afn_preemptive(self._fetch_current_song_mv, song)
