8529112732c4e6ebe7a830c3bc527d216af62044
LWWZH <louis20120127@gmail.com>
2026-07-02 10:51:39 +1000

    fix 2

--- feeluown/gui/uimain/lyric.py
+++ feeluown/gui/uimain/lyric.py
@@ -1,10 +1,9 @@
 import sys
 
-from PyQt6.QtCore import Qt, QRectF, QRect, QSize, QEvent
+from PyQt6.QtCore import Qt, QSize, QEvent
 from PyQt6.QtGui import (
     QPalette,
     QColor,
-    QTextOption,
     QPainter,
     QKeySequence,
     QFont,
@@ -377,19 +376,6 @@ class InnerLyricWindow(QWidget):
         painter.setPen(Qt.PenStyle.NoPen)
         painter.setBrush(self.palette().color(QPalette.ColorRole.Window))
         painter.drawRoundedRect(self.rect(), self._border_radius, self._border_radius)
-
-        # Draw a small handle indicator at the size grip location.
-        # Only show when the grip is visible (multi-line mode).
-        if not self._size_grip.isHidden():
-            painter.save()
-            painter.setPen(QColor("white"))
-            option = QTextOption()
-            option.setAlignment(
-                Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
-            )
-            rect = QRect(self._size_grip.pos(), self._size_grip.size())
-            painter.drawText(QRectF(rect), "●", option)
-            painter.restore()
 
     def setPalette(self, a0: QPalette) -> None:
         super().setPalette(a0)
